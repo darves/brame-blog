8451a4ecd6704433d92992385fa8d3905856df22
Darko Veselinovic <darkov.87@gmail.com>
2022-03-08 10:19:43 +0100

    Project init
    - generate angular app
    - add required modules
    - add some base classes/services

---
+++ src/app/core/resource.api-model.ts
@@ -0,0 +1,31 @@
+export class ResourceGetDTO {
+  id!: number;
+  created_at!: string;
+  updated_at!: string;
+}
+
+export class BaseApiResponse<T = ResourceGetDTO> {
+  data!: T[];
+}
+
+export class ApiResponse<T> extends BaseApiResponse<T> {
+  current_page!: number;
+  first_page_url!: string;
+  from!: number;
+  last_page!: number;
+  last_page_url!: string;
+  next_page_url!: string | null;
+  path!: string;
+  per_page!: number;
+  prev_page_url!: string | null;
+  to!: number;
+  total!: number;
+  links!: ResourceInfo[];
+}
+
+class ResourceInfo {
+  url!: string | null;
+  label!: string;
+  active!: boolean;
+}
+
